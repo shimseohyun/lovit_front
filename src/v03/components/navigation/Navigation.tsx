c4a9fc56b2a953dbbd8545d2760205f6cde2289a
shimseohyun <shim020924@gmail.com>
2026-02-11 21:30:19 +0900

    [Feat] #5 - logout

--- src/v03/components/navigation/Navigation.tsx
+++ src/v03/components/navigation/Navigation.tsx
@@ -1,7 +1,9 @@
 import * as S from "./Navigateion.styled";
 import { useNavigate } from "react-router-dom";
 import { usePostLogout } from "@hooksV03/api/auth";
+import { useAuth } from "@hooksV03/auth/useAuth";
 const Navigation = () => {
+  const { isLoggedIn } = useAuth();
   const { mutate: postLogout } = usePostLogout();
   const navigate = useNavigate();
   const onClickLogo = () => {
@@ -15,7 +17,11 @@ const Navigation = () => {
     <S.Container>
       <S.Content>
         <span onClick={onClickLogo}>lovit</span>
-        <button onClick={onClickLogout}>로그아웃</button>
+        {isLoggedIn ? (
+          <button onClick={onClickLogout}>로그아웃</button>
+        ) : (
+          <div></div>
+        )}
       </S.Content>
     </S.Container>
   );
