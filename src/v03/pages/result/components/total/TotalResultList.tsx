fdb351eb91b18cfdb7641e3be3a512b660bf91f3
shimseohyun <shim020924@gmail.com>
2026-02-26 19:44:19 +0900

    [Feat] #7 - add More

--- src/v03/pages/result/components/total/TotalResultList.tsx
+++ src/v03/pages/result/components/total/TotalResultList.tsx
@@ -1,3 +1,4 @@
+import * as S from "./TotalResult.style";
 import Flex from "@componentsV03/flex/Flex";
 import Label from "@componentsV03/label/Label";
 import { Section } from "@componentsV03/layout/DefaultLayout";
@@ -7,14 +8,18 @@ import { useResultContext } from "@pagesV03/result/context/ResultProvider";
 import { BoardPoint } from "./TotalResult.style";
 import TotalResultCell from "./TotalResultCell";
 import useTotalResultCell from "./useTotalResult";
+import { getItemSummary } from "@dataV03/itemSummary";
+import { useAuth } from "@hooksV03/auth/useAuth";
 
 const TotalResultList = () => {
+  const { isLoggedIn } = useAuth();
   const {
     itemList,
     boardInformation,
     itemSummaryDict,
     itemPositionDict,
     totalBoardDataDict,
+    isMore,
   } = useResultContext();
 
   const { resultDict } = useTotalResultCell({
@@ -25,36 +30,36 @@ const TotalResultList = () => {
     itemSummaryDict: itemSummaryDict,
   });
 
-  // const More = () => {
-  //   if (!itemList || itemList.length === 0) return;
+  const More = () => {
+    if (!itemList || itemList.length === 0) return;
 
-  //   const moreItem = getItemSummary(
-  //     itemList[itemList.length - 1] + 1,
-  //     itemSummaryDict,
-  //   );
+    const moreItem = getItemSummary(
+      itemList[itemList.length - 1] + 1,
+      itemSummaryDict,
+    );
 
-  //   if (moreItem === undefined || moreItem === null) return;
-  //   if (isMore || !isLoggedIn) {
-  //     return (
-  //       <>
-  //         <S.ResultGradient>
-  //           <Label font="body2" color="textLightest">
-  //             아직 분류할 수 있는 인물이 남았어요!
-  //           </Label>
-  //         </S.ResultGradient>
-  //         <TotalResultCell
-  //           key={0}
-  //           itemResult={"???"}
-  //           itemName={moreItem.name}
-  //           itemImg={moreItem.thumbnailURL}
-  //           avgCount={0}
-  //           avg={{ HORIZONTAL: 50, VERTICAL: 50, PREFERENCE: 100 }}
-  //           user={{ HORIZONTAL: 100, VERTICAL: 100, PREFERENCE: 100 }}
-  //         />
-  //       </>
-  //     );
-  //   }
-  // };
+    if (moreItem === undefined || moreItem === null) return;
+    if (isMore || !isLoggedIn) {
+      return (
+        <>
+          <S.ResultGradient>
+            <Label font="body2" color="textLightest">
+              아직 분류할 수 있는 인물이 남았어요!
+            </Label>
+          </S.ResultGradient>
+          <TotalResultCell
+            key={0}
+            itemResult={"???"}
+            itemName={moreItem.name}
+            itemImg={moreItem.thumbnailURL}
+            avgCount={0}
+            avg={{ HORIZONTAL: 50, VERTICAL: 50, PREFERENCE: 100 }}
+            user={{ HORIZONTAL: 100, VERTICAL: 100, PREFERENCE: 100 }}
+          />
+        </>
+      );
+    }
+  };
   return (
     itemList.length > 0 && (
       <>
@@ -87,7 +92,7 @@ const TotalResultList = () => {
               if (!cell) return null;
               return <TotalResultCell key={idx} {...cell} />;
             })}
-          {/* <More /> */}
+          <More />
         </Section>
       </>
     )
